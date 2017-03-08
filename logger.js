ef2dec4ff93e72870f874c6e70b097168c858229
Tom English <redsoxfantom@gmail.com>
2017-03-07 21:16:47 -0500

    configure log level

--- logger.js
+++ logger.js
@@ -6,6 +6,7 @@ module.exports = function(module) {
 var logger = new (winston.Logger)({
    transports: [
       new (winston.transports.Console)({
+         level: config.logLevel,
          formatter: function(options) {
             return options.level.toUpperCase() + ' ['+ module +'] : ' + options.message
          }
